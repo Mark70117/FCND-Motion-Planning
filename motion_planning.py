731b1eecfe8adf0fc69049e2847333519254309c
Mark Anderson <udacity.com@ma7.org>
2018-03-13 17:23:06 -0500

    first pass at merging changes into new code base

--- motion_planning.py
+++ motion_planning.py
@@ -6,6 +6,7 @@ from enum import Enum, auto
 import numpy as np
 
 from planning_utils import a_star, heuristic, create_grid
+from pruning_utils import prune_path
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
@@ -20,6 +21,16 @@ class States(Enum):
     LANDING = auto()
     DISARMING = auto()
     PLANNING = auto()
+
+COLLIDERS_FN = 'colliders.csv'
+
+# rubric key
+# IYPPA-1 -- Implementing Your Path Planning Algorithm // set home position
+# IYPPA-2 -- retrieve current position in geodetic coordinates
+# IYPPA-3 -- change start point to current local position
+# IYPPA-4 -- change goal point
+# IYPPA-5 -- write search algorithm
+# IYPPA-6 -- cull waypoints
 
 
 class MotionPlanning(Drone):
@@ -119,16 +130,34 @@ class MotionPlanning(Drone):
 
         self.target_position[2] = TARGET_ALTITUDE
 
-        # TODO: read lat0, lon0 from colliders into floating point values
-        
-        # TODO: set home position to (lat0, lon0, 0)
+        # IYPPA-1 begin
+        # DONE: read lat0, lon0 from colliders into floating point values
+        with open(COLLIDERS_FN) as f:
+            label = f.readline()
+            latLonStrArr = f.readline().rstrip().split(',')
+            lat0 = float(latLonStrArr[0])
+            lon0 = float(latLonStrArr[1])
 
-        # TODO: retrieve current global position
- 
-        # TODO: convert to current local position using global_to_local()
+        # DONE : set home position to (lat0, lon0, 0)
+        # DONE : CORRECT set home position to (lon0, lat0, 0)
+        self.set_home_position(lon0, lat0, 0)
+        # IYPPA-1 end
+
+
+        # IYPPA-2 begin
+        # DONE: retrieve current global position
+        # print (self.global_home)
+        global_position = [self._longitude, self._latitude, self._altitude]
+        # print(global_position)
+
+        # DONE: convert to current local position using global_to_local()
+        current_local_pos = global_to_local(global_position,self.global_home)
+        # print (current_local_pos)
         
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
+        # IYPPA-2 end
+
         # Read in obstacle map
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=3)
         
@@ -137,22 +166,44 @@ class MotionPlanning(Drone):
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
         grid_start = (north_offset, east_offset)
-        # TODO: convert start position to current position rather than map center
+
+        # IYPPA-3 begin
+        # DONE: convert start position to current position rather than map center
         #start = (int(current_local_pos[0]+north_offset), int(current_local_pos[1]+east_offset))
-        
+        grid_start = (int(current_local_pos[0]+north_offset), int(current_local_pos[1]+east_offset))
+        # IYPPA-3 end
+
         # Set goal as some arbitrary position on the grid
         grid_goal = (north_offset + 10, east_offset + 10)
-        # TODO: adapt to set goal as latitude / longitude position and convert
+        # IYPPA-4 start
+        # DONE: adapt to set goal as latitude / longitude position and convert
+        # 37.795023,-122.400325 fails
+        # 37.793515,-122.397632 works
+        # 37.793375,-122.398791 fails
+        # 37.793490,-122.397895 works
+        # 37.793600,-122.397927 works
+        # 37.793528,-122.398560 crash
+        # 37.793794,-122.396580 crash
+        #goal_local_pos = global_to_local([-122.397927,37.793600,self.global_home[2]],self.global_home)
+        goal_local_pos = global_to_local([-122.402034,37.797330,self.global_home[2]],self.global_home)
+        grid_goal = (int(goal_local_pos[0]+north_offset), int(goal_local_pos[1]+east_offset))
+
+        # IYPPA-4 end
 
         # Run A* to find a path from start to goal
-        # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
+        # DONE: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
+        # IYPPA-5 in a_star [file: planning_utils.py]
         print('Local Start and Goal: ', grid_start, grid_goal)
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         
-        # TODO: prune path to minimize number of waypoints
+        # DONE: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
-
+        # IYPPA-6 begin
+        print ("path: ", path)
+        path = prune_path(path)
+        print ("revised path: ", path)
+        # IYPPA-6 end
         # Convert path to waypoints
         waypoints = [[p[0] - north_offset, p[1] - east_offset, TARGET_ALTITUDE, 0] for p in path]
         # Set self.waypoints
